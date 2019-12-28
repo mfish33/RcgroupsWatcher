2fe30895a53ad4ca09e41d7c108474bfd00ca733
mfish33 <32677537+mfish33@users.noreply.github.com>
2019-12-28 15:58:06 -0500

    fixed formatting

--- circularArray.js
+++ circularArray.js
@@ -1,14 +1,14 @@
 module.exports = class CircularArray {
 	constructor(size) {
-    this.size = size
-		this.arr = []
+		this.size = size;
+		this.arr = [];
 		this.writeHead = 0;
 	}
 
 	write(val) {
-		this.arr.push(val)
-		if(this.arr.length > this.size) {
-      this.arr.shift()
-    }
+		this.arr.push(val);
+		if (this.arr.length > this.size) {
+			this.arr.shift();
+		}
 	}
 };
